fd80a8d8364e5ba7761cf8e58545626af9bfaf6a
Thomas Kleinendorst <thomas@kleinendorst.info>
2025-01-29 12:02:27 +0100

    [AUTO] Nix upgrade: generation: 19, NixOS: 24.11.20250126.4e96537 (2025-01-29T11:02:21Z)

--- configuration.nix
+++ configuration.nix
@@ -99,10 +99,12 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  programs.vim.enable = true;
+  programs.vim.defaultEditor = true;
+  
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     ripgrep
     git
